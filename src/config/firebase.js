ea44edaa65957cfa60722a80b4484e72c79b01ee
Seif Eldin Mahmoud <70539264+kirito28sao@users.noreply.github.com>
2026-04-18 19:31:20 +0200

    disable firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,24 +1,24 @@
 let admin;
 let firebaseInitialized = false;
 
-try {
-  admin = require("firebase-admin");
-
-  if (process.env.FIREBASE_CONFIG) {
-    const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
-
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
-
-    firebaseInitialized = true;
-    console.log("Firebase initialized ✅");
-  } else {
-    console.log("Firebase config not found ❌");
-  }
-
-} catch (error) {
-  console.log("Firebase error ❌", error.message);
-}
+// try {
+//   admin = require("firebase-admin");
+// 
+//   if (process.env.FIREBASE_CONFIG) {
+//     const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+// 
+//     admin.initializeApp({
+//       credential: admin.credential.cert(serviceAccount),
+//     });
+// 
+//     firebaseInitialized = true;
+//     console.log("Firebase initialized ✅");
+//   } else {
+//     console.log("Firebase config not found ❌");
+//   }
+// 
+// } catch (error) {
+//   console.log("Firebase error ❌", error.message);
+// }
 
 module.exports = { admin, firebaseInitialized };
